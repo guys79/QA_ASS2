c207d8563065b1a50438afbc5e8b6d9b7396451b
guy schlesinger <guys797@gmail.com>
2019-12-29 19:25:33 +0200

    FileSystemTest - added some functions there

--- test/system/FileSystemTest.java
+++ test/system/FileSystemTest.java
@@ -1,8 +1,13 @@
 package system;
 
+import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
+import java.io.File;
+import java.nio.file.DirectoryNotEmptyException;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.Random;
 
 import static org.junit.Assert.*;
@@ -19,12 +24,462 @@ public class FileSystemTest {
         this.rand = new Random();
     }
 
+    
+    @Test
+    public void normalDirCreation() throws OutOfSpaceException, BadFileNameException {
+        int parents = rand.nextInt(BOUND);
+        int fileSize = rand.nextInt(this.BOUND);
+        int spaceSize = fileSize + 1 + rand.nextInt(BOUND);
+        FileSystem fileSystem = new FileSystem(spaceSize);
+        String [] path = new String[parents + 1];
+        path[0] = "root";
+        String name = "dirname";
+        for(int i=1;i<path.length;i++)
+        {
+            path[i] = name + i;
+        }
+
+
+        fileSystem.dir(path);
+
+        Tree dir = fileSystem.DirExists(path);
+        assertTrue(dir!=null);
+        assertEquals(dir.name,path[path.length-1]);
+
+
+    }
+
+    private Node getRoot(FileSystem fileSystem) throws OutOfSpaceException, BadFileNameException {
+
+        String [] path = new String[3];
+        path[0] = "root";
+        path[1] = "uniquDirname";
+        path[2] = "fileName";
+
+        //Assuming file creation and fileExist work (there is a test for that)
+        fileSystem.file(path,1);
+        Leaf file = fileSystem.FileExists(path);
+        int count =0;
+        Node curr = file;
+        while(curr.parent != null)
+        {
+
+            assertTrue(count<3);
+            count++;
+            curr  = curr.parent;
+
+        }
+        assertEquals(curr.name,path[0]);
+        return curr;
+
+    }
+    @Test
+    public void dirExists() throws BadFileNameException, OutOfSpaceException {
+        int parents = rand.nextInt(BOUND);
+        int fileSize = rand.nextInt(this.BOUND);
+        int spaceSize = fileSize + 1 + rand.nextInt(BOUND);
+        FileSystem fileSystem = new FileSystem(spaceSize);
+        String [] path = new String[parents + 2];
+        path[0] = "root";
+        String name = "dirname";
+        for(int i=1;i<path.length-1;i++)
+        {
+            path[i] = name + i;
+        }
+        path[path.length-1] = "fileName";
+
+        //Assuming file creation works (there is a test for that)
+        fileSystem.dir(path);
+
+
+        Node curr = getRoot(fileSystem);
+        if(curr.name.equals("root") && curr instanceof Tree && ((Tree)curr).children.containsKey("root"))
+            curr = ((Tree)curr).children.get("root");
+        Tree workingTree;
+        HashMap<String, Node> children;
+        boolean found;
+        for(int i=0;i<path.length - 1;i++){
+
+            assertTrue(curr instanceof Tree);
+            workingTree = (Tree) curr;
+            children = workingTree.children;
+            found  = false;
+            for(Map.Entry<String,Node> child: children.entrySet())
+            {
+                if(child.getKey().equals(path[i+1]))
+                {
+                    curr = child.getValue();
+                    found = true;
+                    break;
+                }
+            }
+
+            assertTrue(found);
+
+        }
+
+        assertTrue(curr instanceof Tree);
+        assertEquals(curr.name,path[path.length-1]);
+
+    }
+
+
+
+
     @Test
     public void creation()
     {
         int spaceSize = rand.nextInt(BOUND);
         FileSystem fileSystem = new FileSystem(spaceSize);
         assertEquals(FileSystem.fileStorage.countFreeSpace(),spaceSize);
+    }
+
+    @Test
+    public void correctFile() throws OutOfSpaceException, BadFileNameException {
+        int parents = rand.nextInt(BOUND);
+        int fileSize = rand.nextInt(this.BOUND);
+        int spaceSize = fileSize + 1 + rand.nextInt(BOUND);
+        FileSystem fileSystem = new FileSystem(spaceSize);
+        String [] path = new String[parents + 1];
+        path[0] = "root";
+        String name = "dirname";
+        for(int i=1;i<path.length-1;i++)
+        {
+            path[i] = name + i;
+        }
+        path[path.length-1] = "fileName";
+        fileSystem.file(path,fileSize);
+
+
+        Space space = FileSystem.fileStorage;
+        Leaf [] allocs = space.getAlloc();
+        Leaf res = null;
+        for(int i=0;i<allocs.length;i++)
+        {
+            if(allocs[i] != null && allocs[i].name.equals(path[path.length-1]))
+                res = allocs[i];
+        }
+        assertTrue(res!=null);
+
+        String [] resPath = res.getPath();
+        assertEquals(resPath.length,path.length);
+        for(int i=0;i<path.length;i++)
+        {
+            assertEquals(resPath[i],path[i]);
+        }
+
+        Node parent = res;
+        int index = path.length -1;
+        while(parent!=null)
+        {
+            if(index == -1)
+            {
+                assertEquals(parent.name,"root");
+                assertEquals(parent.parent,null);
+                break;
+            }
+
+
+            assertEquals(parent.name,path[index]);
+            index -- ;
+            parent = parent.parent;
+        }
+    }
+
+
+    @Test (expected = BadFileNameException.class)
+    public void noRootFile() throws OutOfSpaceException, BadFileNameException {
+        int parents = rand.nextInt(BOUND);
+        int fileSize = rand.nextInt(this.BOUND);
+        int spaceSize = fileSize + 1 + rand.nextInt(BOUND);
+        FileSystem fileSystem = new FileSystem(spaceSize);
+        String [] path = new String[parents + 1];
+        path[0] = "root2";
+        String name = "dirname";
+        for(int i=1;i<path.length-1;i++)
+        {
+            path[i] = name + i;
+        }
+        path[path.length-1] = "fileName";
+        fileSystem.file(path,fileSize);
+    }
+
+    @Test(expected = OutOfSpaceException.class)
+    public void noSpaceNoFileToReplace() throws OutOfSpaceException, BadFileNameException {
+
+        int parents = rand.nextInt(BOUND);
+        int fileSize = rand.nextInt(this.BOUND);
+        int spaceSize = fileSize - 1;
+        FileSystem fileSystem = new FileSystem(spaceSize);
+        String [] path = new String[parents + 1];
+        path[0] = "root";
+        String name = "dirname";
+        for(int i=1;i<path.length-1;i++)
+        {
+            path[i] = name + i;
+        }
+        path[path.length-1] = "fileName";
+        fileSystem.file(path,fileSize);
+    }
+
+
+    private void noSpaceExistFileToReplaceGeneric(int fileSize,int spaceSize) throws OutOfSpaceException, BadFileNameException {
+
+        int parents = rand.nextInt(BOUND);
+        if(spaceSize == fileSize)
+        {
+            fileSize = 2;
+            spaceSize = 3;
+        }
+        FileSystem fileSystem = new FileSystem(spaceSize);
+        String [] path = new String[parents + 1];
+        path[0] = "root";
+        String name = "dirname";
+        for(int i=1;i<path.length-1;i++)
+        {
+            path[i] = name + i;
+        }
+        path[path.length-1] = "fileName";
+        fileSystem.file(path,fileSize);
+        Leaf firstLeaf = fileSystem.FileExists(path);
+        assertTrue(firstLeaf!=null);
+
+
+        fileSystem.file(path,fileSize);
+        Leaf secondLeaf = fileSystem.FileExists(path);
+        assertEquals(secondLeaf.name,firstLeaf.name);
+        assertTrue(secondLeaf!=firstLeaf);
+
+
+    }
+
+    @Test
+    public void noSpaceExistFileToReplace() throws OutOfSpaceException, BadFileNameException {
+        int fileSize = rand.nextInt(this.BOUND) + 1;
+        int spaceSize = (int)(fileSize * 1.5);
+        noSpaceExistFileToReplaceGeneric(fileSize,spaceSize);
+    }
+    @Test
+    public void noSpaceExistFileToReplaceWithALotOfSpace() throws OutOfSpaceException, BadFileNameException {
+        int fileSize = rand.nextInt(this.BOUND) + 1;
+        int spaceSize = fileSize * 10;
+        noSpaceExistFileToReplaceGeneric(fileSize,spaceSize);
+    }
+    
+    
+    
+    @Test(expected = OutOfSpaceException.class)
+    public void noSpaceExistFileToReplaceButDirGiven() throws OutOfSpaceException, BadFileNameException {
+
+        int parents = rand.nextInt(BOUND);
+        int fileSize = rand.nextInt(this.BOUND) + 1;
+        int spaceSize = (int)(fileSize * 1.5);
+        if(spaceSize == fileSize)
+        {
+            fileSize = 2;
+            spaceSize = 3;
+        }
+        FileSystem fileSystem = new FileSystem(spaceSize);
+        String [] path = new String[parents + 1];
+        String [] secondPath = new String[path.length-1];
+        path[0] = "root";
+        secondPath[0] = "root";
+        String name = "dirname";
+        for(int i=1;i<path.length-1;i++)
+        {
+            path[i] = name + i;
+            secondPath[i] = path[i];
+        }
+        path[path.length-1] = "fileName";
+
+
+        fileSystem.file(path,fileSize);
+        Leaf firstLeaf = fileSystem.FileExists(path);
+        assertTrue(firstLeaf!=null);
+
+
+        fileSystem.file(secondPath,fileSize);
+
+
+
+
+    }
+    @Test
+    public void normalFileDelete() throws OutOfSpaceException, BadFileNameException {
+
+        int parents = rand.nextInt(BOUND);
+        int fileSize = rand.nextInt(this.BOUND);
+        int spaceSize = fileSize + 1 + rand.nextInt(BOUND);
+        FileSystem fileSystem = new FileSystem(spaceSize);
+        String [] path = new String[parents + 1];
+        path[0] = "root";
+        String name = "dirname";
+        for(int i=1;i<path.length-1;i++)
+        {
+            path[i] = name + i;
+        }
+        path[path.length-1] = "fileName";
+        fileSystem.file(path,fileSize);
+        fileSystem.rmfile(path);
+        Leaf res = fileSystem.FileExists(path);
+        assertTrue(res == null);
+
+    }
+
+    @Test
+    public void nonExistFileDelete() {
+
+        int parents = rand.nextInt(BOUND);
+        int fileSize = rand.nextInt(this.BOUND);
+        int spaceSize = fileSize + 1 + rand.nextInt(BOUND);
+        FileSystem fileSystem = new FileSystem(spaceSize);
+        String [] path = new String[parents + 1];
+        path[0] = "root";
+        String name = "dirname";
+        for(int i=1;i<path.length-1;i++)
+        {
+            path[i] = name + i;
+        }
+        path[path.length-1] = "fileName";
+
+        fileSystem.rmfile(path);
+        Leaf res = fileSystem.FileExists(path);
+        assertTrue(res == null);
+        assertEquals(FileSystem.fileStorage.countFreeSpace(),spaceSize);
+
+    }
+
+    @Test
+    public void dirNameFileDelete() throws OutOfSpaceException, BadFileNameException, DirectoryNotEmptyException {
+
+        int parents = rand.nextInt(BOUND)+2;
+        int fileSize = rand.nextInt(this.BOUND);
+        int spaceSize = fileSize + 1 + rand.nextInt(BOUND);
+        FileSystem fileSystem = new FileSystem(spaceSize);
+        String [] path = new String[parents + 1];
+        path[0] = "root";
+        String name = "dirname";
+        for(int i=1;i<path.length-1;i++)
+        {
+            path[i] = name + i;
+        }
+        path[path.length-1] = "fileName";
+
+        fileSystem.file(path,fileSize);
+        fileSystem.rmdir(path);
+        Leaf file = fileSystem.FileExists(path);
+        assertTrue(file!=null);
+        assertEquals(file.name,path[path.length-1]);
+
+
+    }
+
+    @Test
+    public void fileNameDirDelete() throws BadFileNameException {
+
+        int parents = rand.nextInt(BOUND);
+        int fileSize = rand.nextInt(this.BOUND);
+        int spaceSize = fileSize + 1 + rand.nextInt(BOUND);
+        FileSystem fileSystem = new FileSystem(spaceSize);
+        String [] path = new String[parents + 1];
+        String [] toDeletePath = new String[parents];
+        path[0] = "root";
+        String name = "dirname";
+        for(int i=1;i<path.length-1;i++)
+        {
+            path[i] = name + i;
+            toDeletePath[i-1] = path[i];
+        }
+        path[path.length-1] = "dirName";
+        fileSystem.dir(path);
+        fileSystem.rmfile(path);
+        Tree file = fileSystem.DirExists(path);
+        assertTrue(file!=null);
+        assertEquals(file.name,path[path.length-1]);
+
+
+    }
+
+    @Test
+    public void FileExists() throws OutOfSpaceException, BadFileNameException {
+
+        int parents = rand.nextInt(BOUND);
+        int fileSize = rand.nextInt(this.BOUND);
+        int spaceSize = fileSize + 1 + rand.nextInt(BOUND);
+        FileSystem fileSystem = new FileSystem(spaceSize);
+        String [] path = new String[parents + 1];
+        path[0] = "root";
+        String name = "dirname";
+        for(int i=1;i<path.length-1;i++)
+        {
+            path[i] = name + i;
+        }
+        path[path.length-1] = "fileName";
+        fileSystem.file(path,fileSize);
+
+        Leaf res = fileSystem.FileExists(path);
+        assertEquals(res.name,path[path.length-1]);
+
+
+    }
+    @Test
+    public void FileDoesNotExist() throws OutOfSpaceException, BadFileNameException {
+
+        int parents = rand.nextInt(BOUND);
+        int fileSize = rand.nextInt(this.BOUND);
+        int spaceSize = fileSize + 1 + rand.nextInt(BOUND);
+        FileSystem fileSystem = new FileSystem(spaceSize);
+        String [] path = new String[parents + 1];
+        path[0] = "root";
+        String name = "dirname";
+        for(int i=1;i<path.length-1;i++)
+        {
+            path[i] = name + i;
+        }
+        path[path.length-1] = "fileName";
+        fileSystem.file(path,fileSize);
+        path[path.length-1] = "fileName2";
+        Leaf res = fileSystem.FileExists(path);
+        assertEquals(res,null);
+
+
+    }
+    @Test
+    public void fileDoesNotExistDirIs() throws OutOfSpaceException, BadFileNameException {
+
+        int parents = rand.nextInt(BOUND) + 1;
+        int fileSize = rand.nextInt(this.BOUND);
+        int spaceSize = fileSize + 1 + rand.nextInt(BOUND);
+        FileSystem fileSystem = new FileSystem(spaceSize);
+        String [] path = new String[parents + 1];
+        path[0] = "root";
+        path[1] = "test";
+        String name = "dirname";
+        for(int i=2;i<path.length-1;i++)
+        {
+            path[i] = name + i;
+        }
+        String [] newPath = new String[2];
+        fileSystem.file(path,fileSize);
+        newPath[0] = path[0];
+        newPath[1] = path[1];
+        Leaf res = fileSystem.FileExists(newPath);
+        assertEquals(res,null);
+
+
+    }
+    // TODO: 29/12/2019 Remove
+    private void print(String [] str)
+    {
+        for(int i=0;i<str.length;i++)
+        {
+            System.out.println(str[i]);
+        }
+    }
+    @After
+    public void clean()
+    {
+        FileSystem.fileStorage = null;
     }
 
 }
